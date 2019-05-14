071536869eab27a7f6254fba86a3552b388f0aea
Osksr Kunicki <oska@informi.eu>
2019-05-14 22:33:52 +0200

    init itoolkit added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-let message = 'Hello webpack example !!!';
-console.log(message);
+    const xt = require("itoolkit");
+    const conn = new xt.iConn("*LOCAL");
+ 
+    const cbJson = (str) => {
+      let result = xt.xmlToJson(str);
+      console.log(JSON.stringify(result, " ", 2))
+    }
+ 
+    let sql = new xt.iSql();  
+    sql.prepare("call qsys2.tcpip_info()");
+    sql.execute();
+    sql.fetch();
+    sql.free();
+    conn.add(sql);
+    conn.run(cbJson);
+
+// --------------------------------------------------------------------
+// for testing purpose it can be used to test building without itoolkit
+// building: npm run build
+// running:  npm run start
+// ---------------------------------------------------------------------
+// let message = 'Hello webpack example !!!';
+// console.log(message);
+
+
