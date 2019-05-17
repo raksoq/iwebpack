ceca5b9ddc3d3fb05843125c1633203dace5be1c
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2019-05-17 12:27:46 -0500

    [webpack.config.js] Update config to fix build errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-var path = require('path');
+const path = require('path');
+const fs = require('fs');
+
+let nodeModules = {};
+
+fs.readdirSync('node_modules')
+  .filter((x) => {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
 
 module.exports = {
+  target: 'node',
   mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
-  }
+  },
+   externals: nodeModules
 };
